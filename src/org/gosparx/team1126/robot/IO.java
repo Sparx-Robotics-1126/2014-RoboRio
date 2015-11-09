573492cc2d8501222d753c7eab67aa21a52625c2
Kmodos <amechler1998@gmail.com>
2015-11-09 18:59:11 -0500

    Drives

--- src/org/gosparx/team1126/robot/IO.java
+++ src/org/gosparx/team1126/robot/IO.java
@@ -5,4 +5,9 @@ public class IO {
 	public static final int DRIVER_LEFT_JOY = 0;
 	public static final int DRIVER_RIGHT_JOY = 1;
 	public static final int OPER_JOY = 2;
+	
+	public static final int PWM_DRIVES_LEFT_FRONT = 0;
+	public static final int PWM_DRIVES_LEFT_BACK = 0;
+	public static final int PWM_DRIVES_RIGHT_FRONT = 0;
+	public static final int PWM_DRIVES_RIGHT_BACK = 0;
 }
--- src/org/gosparx/team1126/robot/subsystem/Controls.java
+++ src/org/gosparx/team1126/robot/subsystem/Controls.java
@@ -11,20 +11,60 @@ public class Controls extends GenericSubsystem implements JoystickListener{
 	private AdvancedJoystick driverLeftJoy;
 	private AdvancedJoystick driverRightJoy;
 	private static Controls controls;
-	
+	private Drives drives;
+
+	//********************************************************************
+	//*******************Driver Controller Mapping**********************
+	//********************************************************************
+	private static final int ATTACK3_Y_AXIS = 1;
+	private static final int ATTACK3_X_AXIS = 0;
+	private static final int ATTACK3_Z_AXIS = 2;
+	private static final int ATTACK3_TRIGGER = 1;
+	private static final int ATTACK3_TOP_BUTTON = 2;
+
+	private static final int NEW_JOY_Y_AXIS = 1;
+	private static final int NEW_JOY_X_AXIS = 0;
+	private static final int NEW_JOY_TRIGGER = 1;//TRIGGEr
+	private static final int NEW_JOY_LEFT = 2;//LEFT
+	private static final int NEW_JOY_RIGHT = 3;//RIGHT
+	private static final int NEW_JOY_MIDDLE = 4;
+
+	//***************************************************************************
+	//***************************XBOX360*****************************************
+	//***************************************************************************
+	private static final int XBOX_A = 1;
+	private static final int XBOX_B = 2;
+	private static final int XBOX_X = 3;
+	private static final int XBOX_Y = 4;
+	private static final int XBOX_L1 = 5;
+	private static final int XBOX_R1 = 6;
+	private static final int XBOX_BACK = 7;
+	private static final int XBOX_START = 8;
+	private static final int XBOX_L3 = 9;
+	private static final int XBOX_R3 = 10;
+	private static final int XBOX_LEFT_X = 0;
+	private static final int XBOX_LEFT_Y = 1;
+	private static final int XBOX_L2 = 2;
+	private static final int XBOX_R2 = 3;
+	private static final int XBOX_RIGHT_X = 4;
+	private static final int XBOX_RIGHT_Y = 5;
+	private static final int XBOX_POV = 0;
+
+
 	public static Controls getInstance(){
 		if(controls == null){
 			controls = new Controls();
 		}
 		return controls;
 	}
-	
+
 	private Controls() {
 		super("Controls", Thread.NORM_PRIORITY);
 	}
 
 	@Override
 	protected boolean init() {
+		drives = Drives.getInstance();
 		operJoy = new AdvancedJoystick("Operator", IO.OPER_JOY);
 		driverLeftJoy = new AdvancedJoystick("Driver Left", IO.DRIVER_LEFT_JOY);
 		driverRightJoy = new AdvancedJoystick("Driver Right", IO.DRIVER_RIGHT_JOY);
@@ -38,7 +78,7 @@ public class Controls extends GenericSubsystem implements JoystickListener{
 
 	@Override
 	protected boolean execute() {
-		
+		drives.setSpeed(driverLeftJoy.getAxis(NEW_JOY_Y_AXIS), driverRightJoy.getAxis(NEW_JOY_Y_AXIS));
 		return false;
 	}
 
@@ -54,7 +94,7 @@ public class Controls extends GenericSubsystem implements JoystickListener{
 
 	@Override
 	public void actionPerformed(ButtonEvent e) {
-		
+
 	}
 
 }
